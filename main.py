a35009bb7808bfa1ef5aea02b1f4cbb3f7cc36ec
Dine512 <dimp.volgin@yandex.ru>
2022-12-09 21:43:48 +0800

    Сделала домашку

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template
+from utils import get_candidate, get_candidates_by_name, get_candidates_by_skill, load_candidates_form_json
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    data = load_candidates_form_json('candidates.json')
+    return render_template('index.html', candidates=data)
+
+
+@app.route('/candidate/<int:x>')
+def candidate_x(x):
+    candidate = get_candidate(x)
+    return render_template('card.html', candidate=candidate)
+
+
+@app.route('/searh/<candidate_name>')
+def search_name(candidate_name):
+    candidates = get_candidates_by_name(candidate_name)
+    return render_template('search.html', candidates=candidates, count=len(candidates))
+
+
+@app.route('/skill/<skill_name>')
+def skill(skill_name):
+    candidates = get_candidates_by_skill(skill_name)
+    return render_template('skill.html', candidates=candidates, count=len(candidates))
+
+
+
+app.run()
+
---
+++ utils.py
@@ -0,0 +1,35 @@
+import json
+
+__data = []
+
+
+def load_candidates_form_json(path):
+    global __data
+    with open(path, 'r', encoding='utf-8') as file:
+        __data = json.load(file)
+    return __data
+
+
+def get_candidate(candidate_id):
+    for candidate in __data:
+        if candidate['id'] == candidate_id:
+            return candidate
+
+
+def get_candidates_by_name(candidate_name):
+    candidates = []
+    for candidate in __data:
+        if candidate_name.lower() in candidate['name'].lower():
+            candidates.append(candidate)
+    return candidates
+
+
+def get_candidates_by_skill(skill_name):
+    candidates = []
+    for candidate in __data:
+        if skill_name in candidate['skills']:
+            candidates.append(candidate)
+    return candidates
+
+load_candidates_form_json('candidates.json')
+print(get_candidates_by_name('leo'))
